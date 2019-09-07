33becc85b48cc6b5f48f4d5f8c56075371c5ab8a
Zach Cossman <Zachcos@gmail.com>
2019-09-07 09:22:58 -0400

    Add styling to display data pulled in from api

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../api';
 
 function Games() {
@@ -20,7 +21,40 @@ function Games() {
     fetchData();
   }, []);
 
-  return <h3>This is the Games component</h3>
+  return (
+    <div>
+      <h1>Most Popular Games</h1>
+      <div className="row">
+        {games.map(game => (
+          <div className="col-lg-4 col-md-6 col-sm-12 mt-5">
+            <div className="card">
+              <img
+                className="card-img-top"
+                src={game.box_art_url}
+                alt={game.name}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{game.name}</h5>
+                <button className="btn btn-success">
+                  <Link
+                    className="link"
+                    to={{
+                      pathname: "game/" + game.name,
+                      state: {
+                        gameID: game.id
+                      }
+                    }}
+                  >
+                    {game.name} streams{" "}
+                  </Link>
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 };
 
 export default Games;
