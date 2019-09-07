b03834f4c034481277cf1e9e5293868b79f5b7a9
Zach Cossman <Zachcos@gmail.com>
2019-09-07 09:33:00 -0400

    Set up api call and format data for use

--- src/components/GameStreams.js
+++ src/components/GameStreams.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../api';
 
 function GameStreams({ match, location }) {
+  const [streamData, setStreamData] = useState([]);
+  const [viewers, setViewers] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await api.get(`https://api.twitch.tv/helix/streams?game_id=${location.state.gameID}`);
+      let dataArray = result.data.data;
+      let finalArray = dataArray.map(stream => {
+        let newURL = stream.thumbnail_url
+          .replace("{width}", "300")
+          .replace("{height}", "300")
+        stream.thumbnail_url = newURL;
+        return stream;
+      })
+
+      let totalViewers = finalArray.reduce((acc, val) => {
+        return acc + val.viewer_count
+      }, 0)
+        setViewers(totalViewers)
+        setStreamData(finalArray)
+    };
+    fetchData();
+  },[location.state.gameID]);
+
   return (
     <div>
       <h3>This is the GameStreams component</h3>
