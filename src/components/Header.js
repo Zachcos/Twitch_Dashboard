79cab55e616ebf429df408cf5c9c3a6d52fd2eed
Zach Cossman <Zachcos@gmail.com>
2019-09-07 09:11:15 -0400

    Import Link element and complete Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
-  return <h3>This is the Header component</h3>
+  return (
+    <nav className="navbar justify-content-center">
+      <li className="nav-item nav-link">
+        <Link to="/">Top Games</Link>
+      </li>
+      <li className="nav-item nav-link">
+        <Link to="/top-streams">Top Streams</Link>
+      </li>
+    </nav>
+  );
 };
 
 export default Header;
