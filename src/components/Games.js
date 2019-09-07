ff9460394431c38be554fd7b39473becaf267ebd
Zach Cossman <Zachcos@gmail.com>
2019-09-07 09:16:00 -0400

    Set up intial api call

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../api';
 
 function Games() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await api.get("https://api.twitch.tv/helix/games/top");
+      console.log(result.data);
+    }
+    fetchData();
+  })
+
   return <h3>This is the Games component</h3>
 };
 
