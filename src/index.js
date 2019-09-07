c4e6d43618516cb438f0db064bc4f16bd2e0067c
Zach Cossman <Zachcos@gmail.com>
2019-09-07 09:12:47 -0400

    Add additional routes to complete Header component routing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Header from './components/Header';
 import Games from './components/Games';
+import Streams from './components/Streams';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'shards-ui/dist/css/shards.min.css';
@@ -10,7 +12,9 @@ import 'shards-ui/dist/css/shards.min.css';
 function App() {
   return (
     <Router>
+      <Header />
       <Route exact path="/" component={Games} />
+      <Route exact path="/top-streams" component={Streams} />
     </Router>
   )
 }
