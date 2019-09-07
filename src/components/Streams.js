980bbc18c87c10460fd4ba5685e1a42f66940888
Zach Cossman <Zachcos@gmail.com>
2019-09-07 09:44:59 -0400

    Add api call to get list of top streams and format data

--- src/components/Streams.js
+++ src/components/Streams.js
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../api';
 
 function Streams() {
+  const [channels, setChannels] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await api.get('https://api.twitch.tv/helix/streams')
+      let dataArray = result.data.data;
+      let gameIDs = dataArray.map(stream => {
+        return stream.game_id
+      })
+      let baseURL = 'https://api.twitch.tv/helix/games?'
+      let queryParams = ''
+      gameIDs.map(id => {
+        return (queryParams = queryParams + `id=${id}&`)
+      });
+
+      let finalURL = baseURL + queryParams;
+      let gameNames = await api.get(finalURL);
+      let gameNameArr = gameNames.data.data;
+      let finalArray = dataArray.map(stream => {
+        stream.gameName = '';
+        gameNameArr.map(name => {
+          if(stream.game_id === name.id) {
+            return stream.gameName = name.name
+          }
+        });
+
+        let newURL = stream.thumbnail_url
+          .replace("{width}", "300")
+          .replace("{height}", "300")
+        stream.thumbnail_url = newURL
+        return stream;
+      });
+      setChannels(finalArray);
+    }
+    fetchData();
+  }, [])
+
   return <h3>This is the Streams component</h3>
 };
 
