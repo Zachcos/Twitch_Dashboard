b4a8caa6f1f2b3d86669873fd45c092950abd7d2
Zach Cossman <Zachcos@gmail.com>
2019-09-07 09:24:21 -0400

    Add styles.css for additional styles and import in index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import Games from './components/Games';
 import Streams from './components/Streams';
 
+import './styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'shards-ui/dist/css/shards.min.css';
 
