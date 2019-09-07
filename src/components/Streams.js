576705a34a85ea08c97c6724c00068562c48b5e7
Zach Cossman <Zachcos@gmail.com>
2019-09-07 09:48:51 -0400

    Add styling to display stream data

--- src/components/Streams.js
+++ src/components/Streams.js
@@ -39,7 +39,36 @@ function Streams() {
     fetchData();
   }, [])
 
-  return <h3>This is the Streams component</h3>
+  return (
+    <div>
+      <h1>Most Popular Live Streams</h1>
+      <div className="row">
+        {channels.map(channel => (
+          <div className="col-lg-4 col-md-6 col-sm-12 mt-5">
+            <div className="card">
+              <img src={channel.thumbnail_url} alt={channel.name} className="card-img-top"/>
+              <div className="card-body">
+                <h3 className="card-title">{channel.user_name}</h3>
+                <h5 className="card-text">{channel.gameName}</h5>
+                <div className="card-text">
+                  {channel.viewer_count} live viewers
+                </div>
+                <button className="btn btn-success">
+                  <a
+                    className="link"
+                    href={"https://twitch.tv/"+ channel.user_name}
+                    target="_blank"
+                  >
+                    watch {channel.user_name}'s stream
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 };
 
 export default Streams;
